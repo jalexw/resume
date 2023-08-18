f8675a0ce00e0d5014e15500ba62d0290c74327c
jalexw <jalexwhitman@gmail.com>
2023-08-18 17:58:15 -0300

    use typescript for watch script

--- watch.ts
+++ watch.ts
@@ -1,7 +1,8 @@
-const chokidar = require('chokidar');
+import { watch } from "chokidar";
+import { exec } from "child_process";
 
 // Files to watch for change
-const watchFiles = [
+const watchFiles: string[] = [
   'resume.tex',
   '_header.tex',
   'TLCresume.sty',
@@ -9,12 +10,11 @@ const watchFiles = [
 ]
 
 // Watch for changes in the files specified above
-const watcher = chokidar.watch(watchFiles)
+const watcher = watch(watchFiles)
 
 function buildPDF() {
   // Execute the 'generate' script in package.json
   // This will run the 'pdflatex' command to generate the PDF
-  const { exec } = require('child_process');
   exec('npm run generate', (err, stdout, stderr) => {
     if (err) {
       // node couldn't execute the command
@@ -28,16 +28,16 @@ function buildPDF() {
 
 // Build PDF once at the start
 buildPDF();
-let lastBuild = Date.now();
+let lastBuildTime: number = Date.now();
 
 watcher.on('all', (event, path) => {
-  if (Date.now() - lastBuild < 1000) {
+  if (Date.now() - lastBuildTime < 1000) {
     // Don't build PDF if it was just built less than a second ago
     return;
   } else {
-    console.log(event, path);
+    console.log(`${event} @ ${path}`);
   }
 
   buildPDF();
-  lastBuild = Date.now();
+  lastBuildTime = Date.now();
 });
